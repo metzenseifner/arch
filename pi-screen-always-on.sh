19646d8459588832fefd7b33cc19385bec2bb014
Agent Schmidt <agent.xp22@googlemail.com>
2017-12-01 21:26:03 +0100

    added no blanking options

--- pi-screen-always-on.sh
+++ pi-screen-always-on.sh
@@ -1,6 +1,9 @@
 # Manually
-xset -display :0.0 -dpms # disable Energy Star (DPMS) features (+dpms enables them)
-xset -display :0.0 s blank # screen-saver control {blank,noblank,off}
-# Add kernel option to set /sys/module/kernel/parameters/consoleblank at boot
+xset -display :0.0 s -dpms # -dpms disable Energy Star (DPMS) features (+dpms enables them)
+xset -display :0.0 s noblank # screen-saver control {blank,noblank,off}
+xset -display :0.0 s off
+xset -display :0.0 s noblank
+
+# Add kernel option to set /sys/module/kernel/parameters/consoleblank at boot pick 1 of 2
 #On Raspberry Pi add consoleblank=0 to /boot/cmdline.txt
 #On grub add GRUB_CMDLINE_LINUX_DEFAULT="quiet splash consoleblank=0"
