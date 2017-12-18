ef401981e318e4e154874fa7ef3548fa1933aee0
Agent Schmidt <agent.xp22@googlemail.com>
2017-12-18 13:36:55 +0100

    Fixed scripted.

--- install-yaourt.sh
+++ install-yaourt.sh
@@ -1,10 +1,17 @@
-mkdir -p "~/devel/build"
-cd "~/devel/build"
-git clone https://aur.archlinux.org/package-query.git
-git clone https://aur.archlinux.org/yaourt.git
+main="${HOME}/devel/build"
+mkdir -p "$main/package-query"
+mkdir -p "$main/yaourt"
 
-cd package-query
-makepkg -si
-cd ../yaourt
-makepkg -si
+if [ -d "$main/package-query" ]; then rm -rf "$main/package-query";fi
+  git clone -v -c http.sslVerify=false https://aur.archlinux.org/package-query.git "$main/package-query"
+if [ -d "$main/yaourt" ]; then rm -rf "$main/yaourt";fi
+  git clone -v -c http.sslVerify=false https://aur.archlinux.org/yaourt.git "$main/yaourt"
+
+echo "==> Installing package-query"
+cd "$main/package-query"
+makepkg -si --noconfirm
+echo "==> Installing yaourt"
+cd "$main/yaourt"
+makepkg -si --noconfirm
+echo "==> Done."
 
